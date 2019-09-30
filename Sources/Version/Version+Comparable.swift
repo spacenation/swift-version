9834919faff92aedde361d195887e5d9a9deaeaa
Alex <alex.buzov@icloud.com>
2019-09-30 16:22:14 -0700

    prerelease and build identifiers

--- Sources/Version/Version+Comparable.swift
+++ Sources/Version/Version+Comparable.swift
@@ -1,11 +1,16 @@
 import Foundation
 
 extension Version: Comparable {
-    
     public static func < (lhs: Version, rhs: Version) -> Bool {
         guard lhs.major >= rhs.major else { return true }
         guard lhs.minor >= rhs.minor else { return true }
         guard lhs.patch >= rhs.patch else { return true }
-        return false
+        return prereleaseCompare(lhs.prerelease, rhs.prerelease)
     }
+}
+
+func prereleaseCompare(_ lhs: [String], _ rhs: [String]) -> Bool {
+    guard lhs.count > rhs.count else { return true }
+    guard lhs.description >= rhs.description else { return true }
+    return false
 }
